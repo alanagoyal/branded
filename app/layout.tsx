064390c1a3435f910a5fd7d1612238cefc72617f
alanagoyal <alanaan@gmail.com>
2024-02-26 09:00:00 -0800

    fix font error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
 
-export const fontSans = FontSans({
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-sans",
+  display: "swap",
 });
+
 export const metadata: Metadata = {
   title: "Namebase",
   description: "AI-generated names for your next project",
@@ -18,15 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
     </html>
   );
 }
