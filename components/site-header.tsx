6588265298fab0b285cec856703680d53374e97a
alanagoyal <alanaan@gmail.com>
2024-03-01 16:40:25 -0800

    undo mobile header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
-
-import { Button, buttonVariants } from "@/components/ui/button";
-import { Icons } from "@/components/icons";
-
-import UserNav from "./user-nav";
-import { createClient } from "@/utils/supabase/server";
 import { ThemeToggle } from "./theme-toggle";
+import { Icons } from "@/components/icons";
+import { createClient } from "@/utils/supabase/server";
+import UserNav from "./user-nav";
+import { Button, buttonVariants } from "@/components/ui/button";
 
 export async function SiteHeader() {
   const supabase = createClient();
@@ -18,47 +16,50 @@ export async function SiteHeader() {
     <nav className="">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <div className="-ml-2 mr-6 flex items-center">
+          <div className="flex items-center">
             <Link href="/">
               <span className="text-2xl font-bold">namebase</span>
             </Link>
           </div>
-          <div className="sm:flex items-center">
-            <Link
-              href="https://github.com/alanagoyal/namebase"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                })}
+          <div className="flex items-center">
+            <div className="hidden sm:flex">
+              <Link
+                href="https://github.com/alanagoyal/namebase"
+                target="_blank"
+                rel="noreferrer"
               >
-                <Icons.gitHub className="h-5 w-5" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            <ThemeToggle />
-            <Link href="/new">
-              {" "}
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.new className="h-5 w-5" />
-                <span className="sr-only">New</span>
-              </div>
-            </Link>
-            {user ? (
-              <UserNav user={user} />
-            ) : (
-              <Link href="login">
-                <Button className="ml-4">Log In</Button>
+                <div
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  <Icons.gitHub className="h-5 w-5" />
+                  <span className="sr-only">GitHub</span>
+                </div>
               </Link>
-            )}
+              <ThemeToggle />
+            </div>
+            <div className="hidden sm:flex items-center">
+              <Link href="/new">
+                <div
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  <Icons.new className="h-5 w-5" />
+                  <span className="sr-only">New</span>
+                </div>
+              </Link>
+              {user ? (
+                <UserNav user={user} />
+              ) : (
+                <Link href="login">
+                  <Button className="ml-4">Log In</Button>
+                </Link>
+              )}
+            </div>
           </div>
         </div>
       </div>
