9212198a653d81c54fca09ddf3a1655647155841
alanagoyal <alanaan@gmail.com>
2024-03-19 13:18:14 -0400

    basic logo generation route

---
+++ app/generate-logo/route.ts
@@ -0,0 +1,29 @@
+// generate-name/route.ts
+import { NextResponse } from "next/server";
+import { OpenAI } from "openai";
+import { init, initLogger, traced, wrapOpenAI } from "braintrust";
+
+const logger = initLogger({ projectName: "namebase" });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(req: Request, res: NextResponse) {
+  try {
+    const body = await req.json();
+    const { name, description } = body;
+
+    const image = await openai.images.generate({ prompt: `A minimalist logo for a company named ${name} whose description is ${description}` });
+
+    console.log(image.data);
+
+    return new Response(JSON.stringify({ response: image }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
--- app/new/page.tsx
+++ app/new/page.tsx
@@ -9,7 +9,7 @@ export default async function GenerateName() {
   return (
     <div className="w-full px-4">
       <h1 className="text-2xl font-bold mb-4">Name Generator</h1>
-      <NameGenerator user={user} names={null}/>
+      <NameGenerator user={user} names={null} />
     </div>
   );
 }
