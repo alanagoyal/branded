1397917070f1278cef42edaaebdfa14dc363585a
alanagoyal <alanaan@gmail.com>
2024-03-25 13:52:36 -0700

    add million lint

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
+const MillionLint = require('@million/lint');
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = MillionLint.next({
+  rsc: true
+})({
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
-        port: '',
-        pathname: '/private/org-g9DVzuADwPF6yY20DMk2rHLx/user-EfSGctBkCrEQElitCVJLZNWA/**',
-      },
-    ],
-  },
-};
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+      port: '',
+      pathname: '/private/org-g9DVzuADwPF6yY20DMk2rHLx/user-EfSGctBkCrEQElitCVJLZNWA/**'
+    }]
+  }
+});
