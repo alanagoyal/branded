97b8518c25d2c29aef72d4058f2b07874f8e2c91
alanagoyal <alanaan@gmail.com>
2024-04-02 10:11:30 -0700

    update error page

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,8 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { AlertCircle, AlertOctagon } from "lucide-react";
+import Link from "next/link";
+
 export default function ErrorPage() {
   return (
-    <div className="w-full px-4 flex justify-center items-center flex-col min-h-screen">
-      <h1 className="text-2xl font-bold mb-4">Sorry, something went wrong</h1>
-      <p>Please make sure you have an account and try again</p>
+    <div
+      className="w-full px-4 flex justify-center items-center flex-col min-h-screen"
+      style={{ marginTop: "-100px" }}
+    >
+      <AlertCircle
+        style={{ width: "20px", height: "20px", marginBottom: "16px" }}
+      />
+      <h1 className="text-2xl text-center font-bold mb-4">
+        Uh oh! An error occurred
+      </h1>
+      <Link className="flex justify-center pt-2" href="/">
+        <Button variant="outline">Back Home</Button>
+      </Link>
     </div>
   );
 }
