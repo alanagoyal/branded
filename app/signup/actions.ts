328ab2656a389f1f65f066e64b1d422356984428
alanagoyal <alanaan@gmail.com>
2024-04-01 21:45:44 -0700

    fix return in else if

--- app/signup/actions.ts
+++ app/signup/actions.ts
@@ -1,41 +1,45 @@
-'use server'
+"use server";
 
-import { revalidatePath } from 'next/cache'
+import { revalidatePath } from "next/cache";
 
-import { createClient } from '@/utils/supabase/server'
-import { SignupFormData } from '@/components/signup-form';
+import { createClient } from "@/utils/supabase/server";
+import { SignupFormData } from "@/components/signup-form";
 
-export async function signup(formData: SignupFormData, idString: string, origin: string) {
-  const supabase = createClient()
+export async function signup(
+  formData: SignupFormData,
+  idString: string,
+  origin: string
+) {
+  const supabase = createClient();
   const { email, password } = formData;
-  const {data, error } = await supabase.auth.signUp({
-    email, 
-    password, 
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
     options: {
-      emailRedirectTo: idString ? origin + `/new?ids=${idString}` : origin + '/new'
-    }
-  })
+      emailRedirectTo: idString
+        ? origin + `/new?ids=${idString}`
+        : origin + "/new",
+    },
+  });
 
   let authError = null;
 
-   // User exists, but is fake. See https://supabase.com/docs/reference/javascript/auth-signup
-   if (data.user && data.user.identities && data.user.identities.length === 0) {
-    console.log("in data user exists thing")
+  // User exists, but is fake. See https://supabase.com/docs/reference/javascript/auth-signup
+  if (data.user && data.user.identities && data.user.identities.length === 0) {
+    console.log("in data user exists thing");
     authError = {
       name: "AuthApiError",
       message: "User already exists",
     };
     return { success: false, errorMessage: authError?.message };
-
   } else if (error) {
     authError = {
       name: error.name,
       message: error.message,
     };
-
+    return { success: false, errorMessage: authError?.message };
   } else {
-    revalidatePath('/', 'layout')
+    revalidatePath("/", "layout");
     return { success: true };
   }
 }
-
