725c1df4a38dfdd1ef637557a5cab3893f8a43b5
alanagoyal <alanaan@gmail.com>
2024-04-01 19:28:06 -0700

    update wordmark to be pink

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -19,7 +19,7 @@ export async function SiteHeader() {
           <div className="flex items-center">
             <Link href="/">
               <Image
-                src="/wordmark_light.png"
+                src="/wordmark_pink.png"
                 alt="namebase"
                 width={196}
                 height={64}
