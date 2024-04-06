0b91096c09f4fd0c42afe3d5d070870d533f946c
alanagoyal <alanaan@gmail.com>
2024-04-06 15:24:33 -0700

    use new generation on share page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { createClient } from "@/utils/supabase/server";
-import { NameGenerator } from "@/components/name-generator";
+import NewGeneration from "@/components/new-generation";
 
 export default async function Names({ params }: { params: { id: string } }) {
   const supabase = createClient();
@@ -16,8 +16,7 @@ export default async function Names({ params }: { params: { id: string } }) {
 
   return (
     <div className="w-full px-4">
-      <h1 className="text-2xl font-bold mb-4">Name Generator</h1>
-      <NameGenerator user={user} names={names}/>
+      <NewGeneration user={user} names={names} />
     </div>
   );
 }
