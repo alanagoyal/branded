c19d264d7eab089eaca42c172334e1730f02c5ff
alanagoyal <alanaan@gmail.com>
2024-04-10 13:46:05 -0700

    update lookup

--- app/find-npm-availability/route.ts
+++ app/find-npm-availability/route.ts
@@ -3,8 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   const packageName = req.nextUrl.searchParams.get("query");
 
+  const encodedNamespace = encodeURIComponent(packageName!).replace('%2F', '%2f');
+
   try {
-    const response = await fetch(`https://npmjs.com/package/${packageName}`);
+    const response = await fetch(`https://registry.npmjs.org/${encodedNamespace}`);
 
     if (response.ok) {
       return new NextResponse(JSON.stringify({ available: false }), {
