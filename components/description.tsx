be79d2f2bf8f02a187f37dbec6c352b58814c77a
alanagoyal <alanaan@gmail.com>
2024-04-11 13:58:19 -0700

    allow enter on input

--- components/description.tsx
+++ components/description.tsx
@@ -5,10 +5,12 @@ import { Input } from "./ui/input";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/navigation";
 
 export default function Description() {
   const [description, setDescription] = useState("");
   const [sessionId, setSessionId] = useState("");
+  const router = useRouter(); // Use the useRouter hook
 
   useEffect(() => {
     const currentSessionId = localStorage.getItem("session_id") || uuidv4();
@@ -18,6 +20,16 @@ export default function Description() {
     setSessionId(currentSessionId);
   }, []);
 
+  const navigateToNew = () => {
+    router.push(`/new?description=${description}&session_id=${sessionId}`);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      navigateToNew();
+    }
+  };
+
   return (
     <div className="w-full md:w-4/5 flex flex-col md:flex-row items-center gap-4">
       <Input
@@ -25,25 +37,20 @@ export default function Description() {
         className="rounded-md w-full md:w-auto md:flex-grow"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
+        onKeyDown={handleKeyPress}
         autoFocus
       />
       <div className="w-full md:w-auto">
-        <Link
-          href={{
-            pathname: "/new",
-            query: { description: description, session_id: sessionId },
+        <Button
+          className="text-lg w-full md:w-auto"
+          style={{
+            background:
+              "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
           }}
+          onClick={navigateToNew} 
         >
-          <Button
-            className="text-lg w-full md:w-auto"
-            style={{
-              background:
-                "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
-            }}
-          >
-            Get Started
-          </Button>
-        </Link>
+          Get Started
+        </Button>
       </div>
     </div>
   );
