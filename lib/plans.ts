434de237aa714e3c1a5b46f350347d5dc69bf6d2
alanagoyal <alanaan@gmail.com>
2024-04-18 14:47:36 -0700

    add preview url

--- lib/plans.ts
+++ lib/plans.ts
@@ -82,7 +82,7 @@ const CustomerPortal = "https://billing.stripe.com/p/login/bIY9BZ71t1uu7cscMM";
 export const PortalLink = isProduction ? CustomerPortal : TestCustomerPortal;
 
 export const baseUrl = isProduction
-  ? "https://branded.ai"
+  ? "https://namebase-git-pricing-changes-basecasevc.vercel.app"
   : "http://127.0.0.1:3000";
 
 export const FreePlanEntitlements = isProduction
